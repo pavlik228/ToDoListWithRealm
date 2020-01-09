a4f410655c34cbe1778b407577685641ea16d5e3
Pavel Aushev <ausevpavel@gmail.com>
2020-01-09 21:06:26 +0300

    Initial Commit

---
+++ ToDoListWithRealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoListWithRealm
+//
+//  Created by Pavel Aushev on 09.01.2020.
+//  Copyright © 2020 Павел Аушев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
